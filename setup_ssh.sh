b52ffdad500f835b33d59b6c27f415633e34a9a0
ryohsuke1231 <136867467+ryohsuke1231@users.noreply.github.com>
2025-11-08 08:49:11 +0000

    kondokoso

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.ssh
-printf "%s" "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
-printf "%s" "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
 chmod 600 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
